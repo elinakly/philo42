0872287e4d240b4f13cf51e17462656a63e5a5f7
Elina Klymova <klimova.elina85@gmail.com>
2025-06-04 14:08:56 +0200

    !0

--- source/ft_atoi.c
+++ source/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: eklymova <eklymova@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 19:21:55 by eklymova          #+#    #+#             */
-/*   Updated: 2025/05/30 19:39:38 by eklymova         ###   ########.fr       */
+/*   Updated: 2025/06/03 18:28:54 by eklymova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_atoi(const char *str)
 		i++;
 	if (str[i] == '+')
 		i++;
-	while (str[i] != '\0' && (str[i] >= '0' && str[i] <= '9'))
+	while (str[i] != '\0' && (str[i] >= '1' && str[i] <= '9'))
 	{
 		result = result * 10 + (str[i] - '0');
 		if (result > INT_MAX || result < INT_MIN)
