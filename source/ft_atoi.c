675e64224609f2ad9d2474a0c9324c3895ff79a3
elinakly <klimova.elina85@gmail.com>
2025-06-17 02:07:58 +0200

    atoi

--- source/ft_atoi.c
+++ source/ft_atoi.c
@@ -12,41 +12,25 @@
 
 #include "../include/philo.h"
 
-// int	ft_atoi(const char *str)
-// {
-// 	int		i;
-// 	long	result;
-
-// 	i = 0;
-// 	result = 0;
-// 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
-// 		i++;
-// 	if (str[i] == '+')
-// 		i++;
-// 	while (str[i] != '\0' && (str[i] >= '0' && str[i] <= '9'))
-// 	{
-// 		result = result * 10 + (str[i] - '0');
-// 		if (result > INT_MAX || result < INT_MIN)
-// 			return (-2);
-// 		i++;
-// 	}
-// 	if (str[i])
-// 		return (-2);
-// 	return (result);
-// }
-
-int	ft_atoi(const char *nptr)
+int	ft_atoi(const char *str)
 {
-	long	sig;
-	long	res;
+	int		i;
+	long	result;
 
-	res = 0;
-	sig = 1;
-	while ((*nptr > 8 && *nptr < 14) || *nptr == ' ')
-		sig = (*nptr++ || res == 0);
-	if (*nptr == 43 || *nptr == 45)
-		sig = (*nptr++ - 44) * -1;
-	while (*nptr >= '0' && *nptr <= '9')
-		res = res * 10 + (*nptr++ - 48);
-	return ((int)(sig * res));
+	i = 0;
+	result = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	if (str[i] == '+')
+		i++;
+	while (str[i] != '\0' && (str[i] >= '0' && str[i] <= '9'))
+	{
+		result = result * 10 + (str[i] - '0');
+		if (result > INT_MAX || result < INT_MIN)
+			return (-2);
+		i++;
+	}
+	if (str[i])
+		return (-2);
+	return (result);
 }
